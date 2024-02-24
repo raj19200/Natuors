5e2f84b1151bd1b11be6c5f7a7672b19c62fadc2
Raj <rajshah0192000@gmail.com>
2024-02-24 17:34:03 -0500

    Connected with mongoDB database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
-
+const DB = process.env.DATABASE_URI.replace('<PASSWORD>', process.env.PASSWORD);
+mongoose.connect(DB).then(() => console.log('Databse connected successfully'));
 const app = require('./app');
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
