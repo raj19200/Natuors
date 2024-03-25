28cbe6452c6cd64e0be4086a058ae2a07501b5ac
Raj <rajshah0192000@gmail.com>
2024-03-25 09:40:24 -0400

    Set Security HTTP headers using helmet package and also add limit to data which is passed in the body

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const helmet = require('helmet');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
+const rateLimit = require('express-rate-limit');
 // Creating route
 const app = express();
-// Middleware _ the use of middleware is simple it is use to do some process before data sending to client.
-app.use(express.json());
+// Middleware _ the use of middleware is use to do some process before data sending to client.
+
+// Global Middleware
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Rate limiter -it is use to limit the user to send request in sprcific time frame.
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 // Creating middleware to access static file in browser.
 app.use(express.static(`${__dirname}/public`));
+
 // Define your own middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
